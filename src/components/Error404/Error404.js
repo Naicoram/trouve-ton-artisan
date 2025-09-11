42921e3983c6306ac09ca81060c1da59176aca2f
Sofiane <maliki.sofiane@gmail.com>
2025-09-11 16:41:15 +0200

    Error 404

--- src/components/Error404/Error404.js
+++ src/components/Error404/Error404.js
@@ -1,11 +1,13 @@
 import './Error404.css';
 
 function Error404() {
-    return(
-        <h1>
-            404
-        </h1>
-    );
+  return (
+    <div className="error404-container">
+      <h1 className="error404-title">404</h1>
+      <p className="error404-text">La page que vous cherchez n’existe pas.</p>
+      <a href="/" className="error404-link">Retour à l’accueil</a>
+    </div>
+  );
 }
 
 export default Error404;
